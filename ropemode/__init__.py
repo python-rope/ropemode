e24f8b3a89e770b829cabdd9c000d45c5cab96c2
Matěj Cepl <mcepl@cepl.eu>
2019-08-06 22:31:09 +0200

    New release, relicense under GPLv3+.

--- ropemode/__init__.py
+++ ropemode/__init__.py
@@ -1,16 +1,22 @@
+# -*- coding: utf-8 -*-
 """ropemode, a helper for using rope refactoring library in IDEs"""
 
 INFO = __doc__
-VERSION = '0.3'
+VERSION = '0.5'
 COPYRIGHT = """\
 Copyright (C) 2007-2012 Ali Gholami Rudi
+Copyright (C) 2013-2019 Matěj Cepl
 
-This program is free software; you can redistribute it and/or modify it
-under the terms of GNU General Public License as published by the
-Free Software Foundation; either version 2 of the license, or (at your
-opinion) any later version.
+This program is free software: you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation, either
+version 3 of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details."""
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this program.  If not, see
+<https://www.gnu.org/licenses/>."""

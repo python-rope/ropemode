cf6f231f8688bbf4ebd9c28f10f292b90438d905
Matěj Cepl <mcepl@cepl.eu>
2019-08-06 22:28:09 +0200

    Hard require setuptools and clean up test suite somewhat.

--- ropemodetest.py
+++ ropemodetest.py
@@ -13,40 +13,40 @@ class ConfigTest(unittest.TestCase):
 
     def test_trivial_case(self):
         action, confs = dialog.show_dialog(_MockAskConfig(['done']), ['done'])
-        self.assertEquals('done', action)
-        self.assertEquals({}, confs)
+        self.assertEqual('done', action)
+        self.assertEqual({}, confs)
 
     def test_asking_normal_configs(self):
         confs = {'name': dialog.Data()}
         minibuffer = _MockAskConfig(['value', 'done'])
         action, result = dialog.show_dialog(minibuffer,
                                             ['done', 'cancel'], confs)
-        self.assertEquals({'name': 'value'}, result)
-        self.assertEquals('done', action)
+        self.assertEqual({'name': 'value'}, result)
+        self.assertEqual('done', action)
 
     def test_optional_confs(self):
         optionals = {'name': dialog.Data()}
         minibuffer = _MockAskConfig(['done'])
         action, result = dialog.show_dialog(minibuffer, ['done', 'cancel'],
                                             optionals=optionals)
-        self.assertEquals(None, result.get('name', None))
-        self.assertEquals('done', action)
+        self.assertEqual(None, result.get('name', None))
+        self.assertEqual('done', action)
 
     def test_optional_confs2(self):
         optionals = {'name': dialog.Data()}
         minibuffer = _MockAskConfig(['name', 'value', 'done'])
         action, result = dialog.show_dialog(minibuffer, ['done', 'cancel'],
                                             optionals=optionals)
-        self.assertEquals({'name': 'value'}, result)
-        self.assertEquals('done', action)
+        self.assertEqual({'name': 'value'}, result)
+        self.assertEqual('done', action)
 
     def test_trivial_batchset(self):
         optionals = {'name': dialog.Data()}
         minibuffer = _MockAskConfig(['batchset', 'name value', 'done'])
         action, result = dialog.show_dialog(minibuffer, ['done', 'cancel'],
                                             optionals=optionals)
-        self.assertEquals({'name': 'value'}, result)
-        self.assertEquals('done', action)
+        self.assertEqual({'name': 'value'}, result)
+        self.assertEqual('done', action)
 
     def test_batchset_multiple_sets(self):
         optionals = {'name1': dialog.Data(), 'name2': dialog.Data()}
@@ -54,8 +54,8 @@ class ConfigTest(unittest.TestCase):
                                      'name1 value1\nname2 value2', 'done'])
         action, result = dialog.show_dialog(minibuffer, ['done', 'cancel'],
                                             optionals=optionals)
-        self.assertEquals({'name1': 'value1', 'name2': 'value2'}, result)
-        self.assertEquals('done', action)
+        self.assertEqual({'name1': 'value1', 'name2': 'value2'}, result)
+        self.assertEqual('done', action)
 
     def test_multiline_sets(self):
         optionals = {'name': dialog.Data()}
@@ -63,8 +63,8 @@ class ConfigTest(unittest.TestCase):
             ['batchset', 'name\n line1\n  line2\n', 'done'])
         action, result = dialog.show_dialog(minibuffer, ['done', 'cancel'],
                                             optionals=optionals)
-        self.assertEquals({'name': 'line1\n line2\n'}, result)
-        self.assertEquals('done', action)
+        self.assertEqual({'name': 'line1\n line2\n'}, result)
+        self.assertEqual('done', action)
 
     def test_complex_batchset(self):
         optionals = {'name1': dialog.Data(), 'name2': dialog.Data(),
@@ -74,10 +74,10 @@ class ConfigTest(unittest.TestCase):
              'line2\n\nname2 value2\n', 'done'])
         action, result = dialog.show_dialog(minibuffer, ['done', 'cancel'],
                                             optionals=optionals)
-        self.assertEquals(
+        self.assertEqual(
             {'name1': 'line1\n line2\n', 'name2': 'value2',
              'name3': 'value3\n'}, result)
-        self.assertEquals('done', action)
+        self.assertEqual('done', action)
 
     def test_skipping_blanks(self):
         optionals = {'name1': dialog.Data(), 'name2': dialog.Data()}
@@ -85,8 +85,8 @@ class ConfigTest(unittest.TestCase):
             ['batchset', '\nname1\n value1\n\nname2 value2\n\n', 'done'])
         action, result = dialog.show_dialog(minibuffer, ['done', 'cancel'],
                                             optionals=optionals)
-        self.assertEquals({'name1': 'value1\n', 'name2': 'value2'}, result)
-        self.assertEquals('done', action)
+        self.assertEqual({'name1': 'value1\n', 'name2': 'value2'}, result)
+        self.assertEqual('done', action)
 
     def test_skip_initial_asking(self):
         confs = {'name': dialog.Data()}
@@ -94,8 +94,8 @@ class ConfigTest(unittest.TestCase):
             ['name', 'value', 'done'])
         action, result = dialog.show_dialog(minibuffer, ['done', 'cancel'],
                                             confs=confs, initial_asking=False)
-        self.assertEquals({'name': 'value'}, result)
-        self.assertEquals('done', action)
+        self.assertEqual({'name': 'value'}, result)
+        self.assertEqual('done', action)
 
     def test_ignoring_trailing_colon_in_config_names(self):
         optionals = {'name1': dialog.Data()}
@@ -103,8 +103,8 @@ class ConfigTest(unittest.TestCase):
             ['batchset', 'name1: value1\n', 'done'])
         action, result = dialog.show_dialog(minibuffer, ['done', 'cancel'],
                                             optionals=optionals)
-        self.assertEquals({'name1': 'value1'}, result)
-        self.assertEquals('done', action)
+        self.assertEqual({'name1': 'value1'}, result)
+        self.assertEqual('done', action)
 
 
 class _MockAskConfig(object):

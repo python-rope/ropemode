1a3be36163273fdac929b67fa9e04dd845e3ac77
Lie Ryan <lie.1296@gmail.com>
2022-07-19 02:14:13 +1000

    Move project metadata to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,26 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name="ropemode",
-    version="0.5",
-    description="a helper for using rope refactoring library in IDEs",
-    author="Ali Gholami Rudi",
-    author_email="aligrudi@users.sourceforge.net",
-    url="https://github.com/python-rope/ropemode",
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    license="GNU GPL",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Operating System :: OS Independent",
-        "Environment :: X11 Applications",
-        "Environment :: Win32 (MS Windows)",
-        "Environment :: MacOS X",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
-        "Natural Language :: English",
-        "Programming Language :: Python",
-        "Topic :: Software Development",
-    ],
-    install_requires=["rope >= 0.9.4"],
-)
+setup()
